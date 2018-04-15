d565154027b8227ab4625454a1eba98f2054eddd
Flupko <35300000+Flupko@users.noreply.github.com>
2018-04-15 22:45:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,8 +2,14 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 bot.on('ready', function () {
-  bot.user.setGame("AntalyaBot, !help");
+  bot.user.setGame("!help");
   console.log("Je suis connecté !")
 })
 
 bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+  if (message.content === '!test') {
+    message.reply('test !')
+  }
+})
