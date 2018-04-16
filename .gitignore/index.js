21f363c3fb41bfa5f357605e7fe2d4d328fb05c1
Flupko <35300000+Flupko@users.noreply.github.com>
2018-04-16 10:04:11 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,10 +10,16 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', function(message) {
 
-  if(message.content === 'Bonjour') {
-      message.reply('Hey')
+  if(message.content === 'Salut') {
+      message.reply('Bonjour')
   }
 
-  if(message.content === 'Salut') {
-    message.reply('Salut')
-}};
+  if(message.content === 'Super bot') {
+      message.channel.sendMessage("Merci, c'est Jysse , mon créateur qui m'a développé ! ;)")
+  }
+
+  if(message.content === 'Qui est la') {
+      message.channel.sendMessage("MOIII")
+  }
+   
+});
