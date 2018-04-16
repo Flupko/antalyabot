cfc03d873c2cd3bfcd36fe68d96d081357df0eac
Flupko <35300000+Flupko@users.noreply.github.com>
2018-04-16 19:25:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -23,3 +23,28 @@ bot.on('message', function(message) {
   }
   
 });
+
+bot.on('message', message => {
+
+  if (message.content.startsWith('!play')) {
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    let args = message.content.split(' ')
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+});
