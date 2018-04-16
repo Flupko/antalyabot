941958a8a0fb778b95eeaf47e730b446f37f8b46
Flupko <35300000+Flupko@users.noreply.github.com>
2018-04-16 13:15:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -23,3 +23,18 @@ bot.on('message', function(message) {
   }
   
 });
+
+client.on("message", (message) => {
+    if (message.content.startsWith("/kick")) {
+        // Easy way to get member object though mentions.
+        var member= message.mentions.members.first();
+        // Kick
+        member.kick().then((member) => {
+            // Successmessage
+            message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
+        }).catch(() => {
+             // Failmessage
+            message.channel.send("Access Denied");
+        });
+    }
+});
