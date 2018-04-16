bb9de770d33ee3689d61a31163a97ecf378bd669
Flupko <35300000+Flupko@users.noreply.github.com>
2018-04-16 14:25:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -26,13 +26,6 @@ bot.on('message', function(message) {
 
 client.on("message", async message => {
 
-  if(message.author.bot) return;
-
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-  
   if(command === "ping") {
     const m = await message.channel.send("Ping?");
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
